b7ba69133d1b45aa50fe578d2b6100ef19967ffb
jm <jm@jmdeMac-mini.local>
2018-09-29 15:45:00 +0800

    更新首页3

--- src/components/homeShouye3.vue
+++ src/components/homeShouye3.vue
@@ -1,8 +1,5 @@
 <template>
 	<div class="homeShouye3">
-		<h3 @click="increment">首页3vuex的数据count</h3>
-		<button  class="btn btn-default" @click="increment">首页3可点击增加vuex的数字</button>
-		<button  class="btn btn-default" @click="decrement">首页3可点击减少vuex的数字</button>
 		<p style="color:blue;font-size:20px">[{{count}}]</p>
 	</div>
 </template>
@@ -10,12 +7,5 @@
 	.homeShouye3{width:100%;height:100%;background:#fff;box-sizing:border-box;padding-top:20px;}
 </style>
 <script>
-    import {mapActions,mapGetters} from "Vuex"
 
-	export default{
-		methods:mapActions(['increment',"decrement"]),
- 		 computed:mapGetters(['count'])
-
-
-	}
 </script>
