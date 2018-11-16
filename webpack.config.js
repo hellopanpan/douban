e9a55dbd8f0f6e93b898bd9e9991f19ced28e0d5
panpan <minp@jumei.com>
2018-11-16 18:18:04 +0800

    change name

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './doulib/dist'),
-    publicPath: '/doulib/dist/',
+    path: path.resolve(__dirname, './doudou/dist'),
+    publicPath: '/doudou/dist/',
     filename: 'build.js'
   },
   module: {
