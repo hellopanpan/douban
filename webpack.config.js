b29dda13e5bac0e776079c7cbce9e6a450317e2e
panmin <minp@jumei.com>
2020-10-21 14:34:48 +0800

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: './',
+    publicPath: 'http://xpanpan.com/douban/dist',
     filename: '[name].js',
   },
   module: {
